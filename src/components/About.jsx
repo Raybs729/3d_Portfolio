9b0cdf52fa19099bb3fa5dfde8f05c9198b94ba7
dev global <raybriones729@gmail.com>
2023-09-11 19:03:42 -0500

    I have added to About section. Formatted paragrapsh using reacts motion library

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,17 +17,28 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] sm:px-10 px-6"
       >
-        I am a Software Developer with skills ranging from Back-end Development,
-        Web Development, and SQL Data-Analytics. With my steadfast drive, I hope
-        to land a job in my dream career field.
+        As a software developer, my skills span back-end development, web development, and SQL data analytics. My journey began with Java programming, which I honed through Tech Elevator’s comprehensive Java Development Bootcamp. This experience significantly enhanced my full-stack development knowledge. Post-graduation, I expanded my skill set by building websites using JavaScript and frameworks like React, Next.js, and Three.js.
       </motion.p>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] sm:px-10 px-6"
+      >
+        In the realm of back-end development, I collaborated with a team of Java developers to create a fitness app; this app leverages PostgreSQL for storing user workouts and authentication. On a personal learning venture, I developed a weather app that uses WeatherAPI to provide real-time weather data based on location; as a fun project, I created a Netflix clone named “Petflix”. This site not only uses the TMDb API to populate it with movies and shows but also features “trailers” of my pets, Ori and Cookie.
+      </motion.p>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] sm:px-10 px-6"
+      >
+        With my relentless drive and curiosity, I am confident in securing a role in my dream career field.
+      </motion.p>
+
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service}/>
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-  paddingX: "sm:px-16 px-6",
+  paddingX: "sm:px-10 px-6",
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
@@ -9,9 +9,9 @@ const styles = {
     "text-[#000000FF] font-medium lg:text-[40px] md:text-[30px] sm:text-[20px] xs:text-[20px] lg:leading-[55px] ",
 
   sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] sm:px-10 px-6",
   sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider sm:px-10 px-6",
 
 };
 
