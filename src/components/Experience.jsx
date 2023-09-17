47a7a10457a39c20366981d46d00ec6b4350aa76
dev global <raybriones729@gmail.com>
2023-09-16 23:26:51 -0500

    adding to experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,22 +1,39 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-import {motion} from 'react-motion'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "react-motion";
 
-import 'react-vertical-timeline-component/style.min.css';
+import "react-vertical-timeline-component/style.min.css";
 
-import {styles} from '../styles'
+import { styles } from "../styles";
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
+const ExperienceCard = ({experience}) => {
+  <VerticalTimelineElement contentStyle=>
+    
+  </VerticalTimelineElement>
+}
+
 const Experience = () => {
   return (
     <>
-      <motion.div variants = {textVariant()}>
-      <p className={styles.sectionSubText}>What I Have Done So Far</p>
-      <h2 className={styles.sectionHeadText}>Projects</h2>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I Have Done So Far</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
       </motion.div>
-    </>
-  )
-}
 
-export default SectionWrapper(Experience, "work")
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience}/>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
