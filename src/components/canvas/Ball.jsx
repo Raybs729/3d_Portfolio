6fa7167209b0b6cd9b3307a301ccf292f8899255
dev global <raybriones729@gmail.com>
2023-09-18 17:26:29 -0500

    added ball textures

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+const Ball = (props) => {
+
+  const [decal] = useTexture([props.imgUrl])
   return (
     <div>Ball</div>
+  
+  )
+}
+
+const BallCanvas = ({icon}) => {
+  return (
+    <Canvas>
+      
+    </Canvas>
   )
 }
 
