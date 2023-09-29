e917db5b603f305a100fcb9bb0756e6a51c49635
dev global <raybriones729@gmail.com>
2023-09-28 20:28:53 -0500

    found fix for bug. incorrect closign tag for label led to compunding errors. fix implemented

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,95 +1,91 @@
-import {useState, useRef} from 'react'
-import { motion } from 'framer-motion';
-import {emailjs} from '@emailjs/browser';
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { emailjs } from "@emailjs/browser";
 
-import { styles } from '../styles';
-import {EarthCanvas} from './canvas'
-import {SectionWrapper} from '../hoc'
-import {slideIn} from '../utils/motion'
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
-const Contact = () => {}
+const Contact = () => {
   const formRef = useRef();
 
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {};
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {};
 
   return (
-    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
-       variants={slideIn('left', 'tween', 0.2, 0.2)}
-       className='flex-[0.75] bg-black-100 p-8 rounded-2xl' 
+        variants={slideIn("left", "tween", 0.2, 0.2)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
-        <p className={styles.sectionSubText}>
-          Get in Touch
-        </p>
+        <p className={styles.sectionSubText}>Get in Touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
-              name='name'
+              type="text"
+              name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="What's your name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
-
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
-            <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Email</span>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               onChange={handleChange}
               placeholder="What's your email?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
-
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
-            <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
-              rows='7'
-              name='message'
+              rows="7"
+              name="message"
               value={form.message}
               onChange={handleChange}
               placeholder="What is your message?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
-
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
           >
-            {loading ? 'Sending...' : 'Send'}
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
       </motion.div>
       <motion.div
-        variants={slideIn('right', 'tween', 0.2, 0.2)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      > 
-        <EarthCanvas/>
-
+        variants={slideIn("right", "tween", 0.2, 0.2)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Contact,"contact");
+export default SectionWrapper(Contact, "contact");
 //= by setting id to "contact" it enables user to click on the contact text in Navbar and have page scroll down to section. same premise used for other related components. "flex flex-col allows for multiple elements to appear one beneath another"
