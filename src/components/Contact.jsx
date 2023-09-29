eaab95865e00d5935c8c56731d741b9743bd77d8
dev global <raybriones729@gmail.com>
2023-09-28 20:18:57 -0500

    encountered error in export default. currently debugging. added form template and earth gltf property. gltf still needs to imported

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ import {EarthCanvas} from './canvas'
 import {SectionWrapper} from '../hoc'
 import {slideIn} from '../utils/motion'
 
-const Contact = () => {
+const Contact = () => {}
   const formRef = useRef();
 
   const [form, setForm] = useState({
@@ -18,17 +18,78 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handeChange = (e) => {}
+  const handleChange = (e) => {}
 
   const handleSubmit = (e) => {}
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+       variants={slideIn('left', 'tween', 0.2, 0.2)}
+       className='flex-[0.75] bg-black-100 p-8 rounded-2xl' 
+      >
+        <p className={styles.sectionSubText}>
+          Get in Touch
+        </p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
 
+            />
+            <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
 
+            />
+            <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows='7'
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What is your message?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+
+            />
+          </label>
+          <button
+            type='submit'
+            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 0.2)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      > 
+        <EarthCanvas/>
+
+      </motion.div>
     </div>
   )
 }
 
-export default SectionWrapper(Contact
+export default SectionWrapper(Contact,"contact");
+//= by setting id to "contact" it enables user to click on the contact text in Navbar and have page scroll down to section. same premise used for other related components. "flex flex-col allows for multiple elements to appear one beneath another"
